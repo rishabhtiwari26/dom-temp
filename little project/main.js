9253d267f0965c2f96e991e2b13104b9d224f975
= <rishabhtiwari26>
2023-09-05 18:18:59 +0530

    delete

--- little project/main.js
+++ little project/main.js
@@ -10,10 +10,26 @@ function add(e){
     let obj_ser=JSON.stringify(obj)
     localStorage.setItem(obj.email,obj_ser)
     let obj_deser=JSON.parse(localStorage.getItem(obj.email))
-    console.log(obj_deser)
+
     let li = document.createElement('li')
     li.className='by-email'
+    
+    let btn=document.createElement('button');
+    btn.appendChild(document.createTextNode('Delete'))
+    
+    
+    
     let li_value=document.createTextNode(obj_deser.Name+' '+obj_deser.email+' '+obj_deser.mobile)
+    
     li.appendChild(li_value)
+    li.appendChild(btn)
     document.body.appendChild(li)
+
+    btn.addEventListener('click',del);
+    function del(event){
+        
+        let li = event.target.parentNode
+        let pli=li.parentNode
+        pli.removeChild(li)
+    }
 }
